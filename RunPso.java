201302ef3607d64f0b364e99384264492163630e
szenius <ting.szeying@gmail.com>
2018-03-22 21:17:32 +0800

    Realized that RunPso is just running more evolutions

--- RunPso.java
+++
@@ -1,8 +0,0 @@
-public class RunPso {
-	public static void main(String[] args) {
-		int numRuns = Integer.parseInt(args[0]);
-		for (int i = 0; i < numRuns; i++) {
-			PSO.main(new String[0]);
-		}
-	}
-}
