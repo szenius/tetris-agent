198ecc3438689fc78825a883e5c298f7e23980e5
nbriannl <neilbrian.nl@gmail.com>
2018-03-22 13:09:17 +0800

    Each game aka a simulation now prints it's performance

--- Simulation.java
+++ Simulation.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.concurrent.Callable;
 
 class Simulation implements Callable<Integer> {
@@ -31,8 +32,13 @@ class Simulation implements Callable<Integer> {
                 e.printStackTrace();
             }
         }
+		System.out.println("For " + generatePositionKey(weightSets) + ": " + s.getRowsCleared());
         return s.getRowsCleared();
 	}
 
+	// Return string version of array of weights as String or key for use in Maps
+	private String generatePositionKey(double[] position) {
+		return Arrays.toString(position);
+	}
 
 }
