f7108f7a708ee1fc44ea0bb0e2531ac37ca6d52e
szenius <ting.szeying@gmail.com>
2018-03-24 22:16:56 +0800

    No need to input feature bits since script takes care of all combis

--- RunPSO.java
+++ RunPSO.java
@@ -2,12 +2,8 @@ public class RunPSO {
 	public static void main(String[] args) {
 		if (args.length < 1) {
 			System.out.println("Not enough arguments.");
-			System.out.println("Usage: java RunPSO <num_runs> <(optional) use_old_features> <(optional) feature_bits>");
+			System.out.println("Usage: java RunPSO <num_runs> <(optional) use_old_features>");
 			System.exit(-1);
-		}
-		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
-			PSO.main(new String[]{"false", "true"});
-			PSO.main(new String[]{"true", "false"});
 		}
 
 		if (args.length > 1) {
@@ -24,6 +20,11 @@ public class RunPSO {
 				for (int i = 1; i < Math.pow(2, Heuristic.INDEX_NEW_FEATURES.length); i++) {
 					PSO.main(new String[]{"false", "true", bitStringStart + Integer.toBinaryString(i) + bitStringEnd});
 				}
+			}
+		} else {
+			for (int i = 0; i < Integer.parseInt(args[0]); i++) {
+				PSO.main(new String[]{"false", "true"});
+				PSO.main(new String[]{"true", "false"});
 			}
 		}
 	}
