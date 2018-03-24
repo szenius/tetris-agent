66ea8a297b4155b8657939841eb64dd57beaf1a9
szenius <ting.szeying@gmail.com>
2018-03-24 21:38:55 +0800

    Script to run PSO automatically

---
+++ RunPSO.java
@@ -0,0 +1,30 @@
+public class RunPSO {
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.out.println("Not enough arguments.");
+			System.out.println("Usage: java RunPSO <num_runs> <(optional) use_old_features> <(optional) feature_bits>");
+			System.exit(-1);
+		}
+		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
+			PSO.main(new String[]{"false", "true"});
+			PSO.main(new String[]{"true", "false"});
+		}
+
+		if (args.length > 1) {
+			if (Boolean.parseBoolean(args[1])) {
+				// use old features + combi of new features
+				String bitString = "11111";
+				for (int i = 1; i < Math.pow(2, Heuristic.INDEX_OLD_FEATURES.length); i++) {
+					PSO.main(new String[]{"true", "false", bitString + Integer.toBinaryString(i)});
+				}
+			} else {
+				// use new features + combi of old features
+				String bitStringStart = "1";
+				String bitStringEnd = "11111";
+				for (int i = 1; i < Math.pow(2, Heuristic.INDEX_NEW_FEATURES.length); i++) {
+					PSO.main(new String[]{"false", "true", bitStringStart + Integer.toBinaryString(i) + bitStringEnd});
+				}
+			}
+		}
+	}
+}
