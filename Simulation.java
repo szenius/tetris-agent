664b9ea05107c54a9fade44e3045bf4fc8b4e1d3
Bryan <bryankoh137@hotmail.com>
2018-04-20 13:15:41 +0800

    Updated Genetic Basic Algorithm

--- Simulation.java
+++ Simulation.java
@@ -1,10 +1,15 @@
 import java.util.concurrent.Callable;
 import java.util.Arrays;
 
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
 class Simulation implements Callable<Integer> {
 	private Heuristic h;
 	private double[] weightSets;
 	private int numRepetitions;
+
+	private Logger LOGGER = Logging.getInstance();
 
 	public Simulation() {
 		h = new Heuristic(false, true);
@@ -23,23 +28,34 @@ class Simulation implements Callable<Integer> {
 		this.weightSets = weightSets;
 		this.numRepetitions = numRepetitions;
 	}
-
-
+	
 	public Integer call() {
 		double[] results = new double[numRepetitions];
-		double sum = 0;
+		//double sum = 0;
 		for (int i = 0; i < numRepetitions; i++) { // compute sum
 			results[i] = playGame();
-			sum += results[i];
+			//sum += results[i];
 		}
-		double mean =  sum / numRepetitions;
-		double sumSqDiff = 0;
-		for (int i = 0; i < numRepetitions; i++) { // compute std dev
-			sumSqDiff += Math.pow((results[i] - mean), 2);
+		Arrays.sort(results);
+		int middle = numRepetitions / 2;
+		double score = 0;
+		if(numRepetitions % 2 == 0) {
+			score = (results[middle-1] + results[middle]) / 2;
+		} else {
+			score = results[middle];
 		}
-		double score = sum / Math.sqrt(sumSqDiff / numRepetitions) * 1000; // compute final score (= sum / std dev * 1000)
-		System.out.println("Weights: " + Arrays.toString(weightSets) + "\n" +
-			"Results: " + Arrays.toString(results) + " = " + (int) score + "; average = " + (int) mean);
+		
+		//double mean =  sum / numRepetitions;
+		//double sumSqDiff = 0;
+		//for (int i = 0; i < numRepetitions; i++) { // compute std dev
+		//	sumSqDiff += Math.pow((results[i] - mean), 2);
+		//}
+		//double score = sum / Math.sqrt(sumSqDiff / numRepetitions) * 1000; // compute final score (= sum / std dev * 1000)
+		//System.out.println("Weights: " + Arrays.toString(weightSets) + "\n" +
+			//"Results: " + Arrays.toString(results) + " = " + (int) score + "; average = " + (int) mean);
+		
+		//LOGGER.info("Weights: " + Arrays.toString(weightSets) + " ==> " +
+		//	"Results: " + Arrays.toString(results) + " = " + (int) score);
 		return (int) score;
     }
 
@@ -50,21 +66,25 @@ class Simulation implements Callable<Integer> {
 	public int playGame() {
 		PlayerSkeleton p = new PlayerSkeleton();
         State s = new State();
+        //TFrame f = new TFrame(s);
         while(!s.hasLost()) {
         	try {
             	s.makeMove(p.pickMove(s, s.legalMoves(), weightSets, h));
+            	//s.draw();
+            	//s.drawNext(0, 0);
             } catch (Exception e) {
             	e.printStackTrace();
+            	//LOGGER.log(Level.SEVERE, "an exception was thrown" , e);
             	return -1;
             }
             try {
                 Thread.sleep(0);
             } catch (InterruptedException e) {
                 e.printStackTrace();
+                //LOGGER.log(Level.SEVERE, "an exception was thrown" , e);
             }
         }
+        //f.dispose();
         return s.getRowsCleared();
 	}
-
-
 }
